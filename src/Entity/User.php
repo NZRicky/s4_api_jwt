308b316ce77e508850cf0a1bb1cfbf2c08d7258d
Ricky <nzrickyli@gmail.com>
2019-09-14 15:07:15 +0000

    add fos user bundle & user entity

---
+++ src/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ricky
+ * Date: 15/9/19
+ * Time: 2:58 AM
+ */
+
+namespace App\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
